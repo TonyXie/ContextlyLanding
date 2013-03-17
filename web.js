1eb716cb4a6b2a8f45697219123ae1707156b329
TonyXie <txie145@gmail.com>
2013-03-17 13:40:58 -0400

    back to full throttle

--- web.js
+++ web.js
@@ -22,11 +22,40 @@ app.get('/', function(request, response) {
   });
 });
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+// link to the public directory
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function(client) {
+  console.log("Someone's here...");
+
+  client.on('join', function(nickname) {
+    client.set('nickname', nickname);
+    console.log(nickname + " has joined the room");
+    names.push(nickname);
+    client.broadcast.emit("new_user", nickname);
+    messages.forEach(function(message) {
+      client.emit("message", message);
+    });
+    client.emit("join", names);
+  });
+
+  client.on('chat', function(message) {
+    messages.push(message);
+    client.broadcast.emit('message', message);
+    console.log('current list of all messages: ' + messages);    
+  })
+
+   // deal with disconnect
+  client.on('disconnect', function(name) {
+    client.get('nickname', function(err, name) {
+      client.broadcast.emit("remove_chatter", name);
+    });
+  });
+
 });
 
 var port = process.env.PORT || 3000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
