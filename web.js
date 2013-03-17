7977bc497d3927c8e0dfe26f6c5843447ecdd747
TonyXie <txie145@gmail.com>
2013-03-17 13:25:30 -0400

    initial

--- web.js
+++ web.js
@@ -1,12 +1,32 @@
-var express = require('express');
+require('coffee-script');
 
-var app = express.createServer(express.logger());
+var $, EventEmitter, app, express, http, io, logger, names, socket;
+
+express = require('express');
+EventEmitter = require('events').EventEmitter;
+http = require('http');
+socket = require('socket.io');
+$ = require('jquery');
+logger = new EventEmitter();
+app = express.createServer(express.logger());
+io = socket.listen(app);
+
+names = [];
+messages = [];
+
+app.set('view options', {layout: false});
+
+app.get('/', function(request, response) {
+  return response.render(__dirname + "/index.ejs", {
+    names: names
+  });
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
