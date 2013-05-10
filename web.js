ba6c2191a75ba7e55ea347302958d227a79aa952
TonyXie <txie145@gmail.com>
2013-05-09 20:55:32 -0700

    more design

--- web.js
+++ web.js
@@ -1,64 +1,18 @@
-require('coffee-script');
+var express = require('express');
 
-var $, EventEmitter, app, express, http, io, logger, names, socket;
-
-express = require('express');
-EventEmitter = require('events').EventEmitter;
-http = require('http');
-socket = require('socket.io');
-$ = require('jquery');
-logger = new EventEmitter();
-app = express.createServer(express.logger());
-io = socket.listen(app);
-
-// configuration for heroku, since heroku does not support web sockets
-io.configure(function() {
-  io.set("transports", ["xhr-polling"]);
-  return io.set("polling duration", 10);
-});
-
-names = []
-
-messages = []
+var app = express.createServer(express.logger());
 
 app.set("view options", {
   layout: false
 });
 
-app.get("/", function(request, response) {
-  return response.render(__dirname + "/index.ejs", {
-    names: names
-  });
+app.use(express.static(__dirname)); // This line.
+
+app.get('/', function(request, response) {
+  response.render(__dirname + "/index.ejs");
 });
 
-app.use(express["static"](__dirname + "/public"));
-
-io.sockets.on("connection", function(client) {
-  console.log("Someone's here...");
-  client.on("join", function(nickname) {
-    client.set("nickname", nickname);
-    console.log(nickname + " has joined the room");
-    names.push(nickname);
-    client.broadcast.emit("new_user", nickname);
-    messages.forEach(function(message) {
-      return client.emit("message", message);
-    });
-    return client.emit("join", names);
-  });
-  client.on("chat", function(message) {
-    messages.push(message);
-    client.broadcast.emit("message", message);
-    return console.log("current list of all messages: " + messages);
-  });
-  return client.on("disconnect", function(name) {
-    return client.get("nickname", function(err, name) {
-      return client.broadcast.emit("remove_chatter", name);
-    });
-  });
-});
-
-port = process.env.PORT || 3000;
-
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  return console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
